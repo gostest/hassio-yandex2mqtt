abd3f6f5068696ada1cd71f782321e2468a1bfc3
dparhonin <deps@yandex.ru>
2020-05-01 00:20:24 +0300

    Debug is enabled now.

--- yandex2mqtt/run.sh
+++ yandex2mqtt/run.sh
@@ -2,7 +2,7 @@
 
 CONFIG_PATH=/data/options.json
 DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
-DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
+#DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
 
 # Check if config exists already
 mkdir -p $DATA_PATH
@@ -10,8 +10,5 @@ mkdir -p $DATA_PATH
 # Parse config
 cp $CONFIG_PATH .
 
-# Link shared config
-ln -s $CONFIG_PATH /config/yandex2mqtt.json
-
 # RUN yandex2mqtt
-YANDEX2MQTT_DATA="$DATA_PATH" pm2-runtime start npm -- start
+YANDEX2MQTT_DATA="$DATA_PATH" DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH) pm2-runtime start npm -- start
