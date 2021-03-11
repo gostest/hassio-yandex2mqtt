124686e3a1ff355bf16b3251b2544099841fd541
Dmitry Parkhonin <deps@yandex.ru>
2021-03-11 19:49:25 +0000

    Configs fixed.

--- yandex2mqtt/run.sh
+++ yandex2mqtt/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 CONFIG_PATH=/data/options.json
-DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
-
-# Check if config exists already
-mkdir -p $DATA_PATH
-
-# Parse config
-cp $CONFIG_PATH .
 
 # RUN yandex2mqtt
-YANDEX2MQTT_DATA="$DATA_PATH" DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH) pm2-runtime start npm -- start
+DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH) pm2-runtime start npm -- start
